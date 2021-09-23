9b08e38bb7280ad336dacda02036b432e40c7830
AndreBa <Andre_Barrientos@ultimatesoftware.com>
2021-09-23 09:33:16 -0400

    Finished up modal

--- Web Component & Stencil/components/my-modal/modal.js
+++ Web Component & Stencil/components/my-modal/modal.js
@@ -3,8 +3,15 @@ template.innerHTML = `
   <link rel="stylesheet" href="style.css">
   <div id="backdrop"></div>
   <div id="modal-content">
-    <h1><slot>Default Header</slot></h1>
-    <p><slot name="content">Default</slot></p>
+    <header>
+      <slot name="title">Default Header</slot>
+    </header>
+    <div id="content">
+      <slot name="content">Default</slot>
+    </div>
+    <div id="footer">
+      <button id="close-btn">Close</button>
+    </div>
   </div>
 `;
 
@@ -13,46 +20,33 @@ class Modal extends HTMLElement {
     super();
     this._header = "Dummy header";
     this._content = "Dummy content";
-    this._isVisbile = false;
+    this.isOpen = false;
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.addEventListener("click", (ev) => {
-      console.log(ev.target);
-    });
+    this.shadowRoot
+      .querySelector("#footer")
+      .querySelector("#close-btn")
+      .addEventListener("click", this.close.bind(this));
+    this.shadowRoot
+      .querySelector("#backdrop")
+      .addEventListener("click", this.close.bind(this));
   }
 
   connectedCallback() {
-    // if (this.hasAttribute("isVisible")) {
-    //   this._isVisbile = true;
-    // }
-    // this.shadowRoot
-    //   .querySelector("#backdrop")
-    //   .addEventListener("click", (ev) => {
-    //     this._isVisbile = false;
-    //     this.style.display = "none";
-    //   });
-    // this._render();
+    if (this.hasAttribute("opened")) {
+      this.open();
+    }
   }
 
-  // attributeChangedCallback(name, oldValue, newValue) {
-  //   if (name === "is-visible") {
-  //     this._isVisbile = newValue !== null ? true : false;
-  //   }
+  open() {
+    this.setAttribute("opened", "");
+    this.isOpen = true;
+  }
 
-  //   this._render();
-  // }
-
-  // static get observedAttributes() {
-  //   return ["is-visible"];
-  // }
-
-  // _render() {
-  //   if (this._isVisbile) {
-  //     this.style.display = "block";
-  //   } else {
-  //     this.style.display = "none";
-  //   }
-  // }
+  close() {
+    this.removeAttribute("opened");
+    this.isOpen = false;
+  }
 }
 
 customElements.define("sf-modal", Modal);
